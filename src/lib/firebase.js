4e1686719a00f024fe39cb701d2a404a67a81f38
diegodona36-netizen <diegodona36@gmail.com>
2026-08-13 14:35:48 -0600

    fix: update Firebase credentials to mstore-7b427

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBihtpPaMgJZ2Uiq4OlxIX1YYmW_nqJi6M",
-  authDomain: "mstore-ae718.firebaseapp.com",
-  projectId: "mstore-ae718",
-  storageBucket: "mstore-ae718.firebasestorage.app",
-  messagingSenderId: "573877944655",
-  appId: "1:573877944655:web:50c2b0b1de5b54493d853a",
-  measurementId: "G-J2NQY29LTC"
+  apiKey: "AIzaSyBnF5n9IaSFXrbuzsRj6qCg-OK01JxpMr0",
+  authDomain: "mstore-7b427.firebaseapp.com",
+  projectId: "mstore-7b427",
+  storageBucket: "mstore-7b427.firebasestorage.app",
+  messagingSenderId: "820330414177",
+  appId: "1:820330414177:web:a4db020834817a3112ea20",
+  measurementId: "G-9M28MV90QV"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
